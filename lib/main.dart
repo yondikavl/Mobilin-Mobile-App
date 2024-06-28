0ae680b900d9cb71c174d761090db7ab46cff8ca
yondikavl <yondikaviolanda@gmail.com>
2024-06-29 02:13:37 +0700

    "fix: models"

--- lib/main.dart
+++ lib/main.dart
@@ -8,8 +8,34 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Mugiwara Crew",
+      title: 'Strawhat Pirate',
       theme: ThemeData(),
+      home: const DetailScreen(),
+    );
+  }
+}
+
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              margin: const EdgeInsets.only(top: 16.0),
+              child: const Text(
+                'Strawhat pirate',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/models/car.dart
@@ -0,0 +1,15 @@
+class Car {
+  final String id;
+  final String name;
+  final String description;
+  final double pricePerDay;
+  final String imageUrl;
+
+  Car({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.pricePerDay,
+    required this.imageUrl,
+  });
+}
